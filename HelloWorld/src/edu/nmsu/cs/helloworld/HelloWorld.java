f79934729e4b81dc54fb2dfa6d41afab16edb6ac
nhat15032002@gmail.com <nhat15032002@gmail.com>
2022-01-23 18:51:52 -0700

    create new project HelloWorld.src.edu.nmsu.cs.helloworld, Questions folder include p1Questions.txt and p2Questions.txt and add 2 Ant buildfiles in /Circles and /SimpleWebServer

---
+++ HelloWorld/src/edu/nmsu/cs/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+//Nate Le
+//1/21/2022
+//CS371 
+//Lab 1
+
+package edu.nmsu.cs.helloworld;
+class HelloWorld
+{
+
+	public static void main(String args[])
+	{
+		System.out.println("Hello World!");
+	}
+}
